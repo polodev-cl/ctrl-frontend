f158fe3c7cf411b8b5c308e9a1d5d82fea9f4329
Estudiappcl <estudiapp2022@gmail.com>
2024-03-26 15:23:58 -0300

    test amplify

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CtrlA
+# CtrlaAmplify
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { Router } from '@angular/router'; // Importa el Router
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let router: Router; // Declara una variable para el Router
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
-  });
-
-  beforeEach(() => {
-    router = TestBed.inject(Router); // Obtiene la instancia del Router
   });
 
   it('should create the app', () => {
@@ -27,21 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render the button', () => {
+  it(`should have the 'ctrla-amplify' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ctrla-amplify');
+  });
+
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('button')).toBeTruthy();
-    expect(compiled.querySelector('button')?.textContent).toContain('Ir a Login');
-  });
-
-  it('should redirect to login when button is clicked', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const navigateSpy = spyOn(router, 'navigateByUrl'); // Utiliza router en lugar de app.router
-    fixture.detectChanges();
-    const button = fixture.nativeElement.querySelector('button');
-    button.click();
-    expect(navigateSpy).toHaveBeenCalledWith('/login'); // Utiliza navigateByUrl en lugar de navigate
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ctrla-amplify');
   });
 });

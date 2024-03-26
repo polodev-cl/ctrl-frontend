ad27cbc26c723a6e603d8c7d1391cef8a9807dd0
Estudiappcl <estudiapp2022@gmail.com>
2024-03-26 16:44:45 -0300

    version funcional en codecommit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router'; // Importa el Router
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let router: Router; // Declara una variable para el Router
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    router = TestBed.inject(Router); // Obtiene la instancia del Router
   });
 
   it('should create the app', () => {
@@ -14,16 +27,21 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'amplify-repo' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('amplify-repo');
-  });
-
-  it('should render title', () => {
+  it('should render the button', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, amplify-repo');
+    expect(compiled.querySelector('button')).toBeTruthy();
+    expect(compiled.querySelector('button')?.textContent).toContain('Ir a Login');
+  });
+
+  it('should redirect to login when button is clicked', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const navigateSpy = spyOn(router, 'navigateByUrl'); // Utiliza router en lugar de app.router
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    expect(navigateSpy).toHaveBeenCalledWith('/login'); // Utiliza navigateByUrl en lugar de navigate
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'amplify-repo';
+
+  constructor(private router: Router) {}
+
+  redirectToLogin() {
+    this.router.navigate(['/login']);
+  }
 }

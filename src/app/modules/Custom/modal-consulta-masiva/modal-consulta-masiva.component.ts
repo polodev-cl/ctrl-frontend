79185930dc9abbe7f099b7cf64c2388663d9750a
Pinkq1 <diegomedina99j@gmail.com>
2024-04-25 14:03:00 -0400

    autocomplete company in modal consulta masiva

--- src/app/modules/Custom/modal-consulta-masiva/modal-consulta-masiva.component.ts
+++ src/app/modules/Custom/modal-consulta-masiva/modal-consulta-masiva.component.ts
@@ -1,24 +1,61 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+import { CompanyService, Company } from '../../../services/company.service';
+import { Observable } from 'rxjs';
+import { map, startWith, switchMap, debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'app-modal-consulta-masiva',
   templateUrl: './modal-consulta-masiva.component.html',
-  styleUrls: [ './modal-consulta-masiva.component.css' ],
+  styleUrls: ['./modal-consulta-masiva.component.css'],
   standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
 })
 export class ModalConsultaMasivaComponent {
-
   @Output() cerrar = new EventEmitter<void>();
 
-  constructor(private router: Router) {
+  empresaControl = new FormControl();
+  empresasFiltradas: Observable<Company[]> | undefined;
+
+  constructor(private router: Router, private companyService: CompanyService) {}
+
+  ngOnInit(): void {
+    this.empresasFiltradas = this.empresaControl.valueChanges.pipe(
+      startWith(''),
+      switchMap((value) => this.filterCompanies(value))
+    );
+  }
+  private filterCompanies(value: string): Observable<Company[]> {
+    return this.companyService
+      .getCompanies()
+      .pipe(
+        map((companies) =>
+          companies.filter((company: Company) =>
+            company.razonSocial.toLowerCase().includes(value.toLowerCase())
+          )
+        )
+      );
   }
 
+  seleccionarEmpresa(empresa: Company) {
+    console.log('Empresa seleccionada:', empresa);
+  }
   cerrarModal(): void {
     this.cerrar.emit();
   }
 
   irAConsultaMasiva() {
-    this.router.navigate([ '/consulta-masiva' ]);
+    this.router.navigate(['/consulta-masiva']);
   }
 }
--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+export interface Company {
+  id: number;
+  razonSocial: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
-  private baseUrl = 'https://3b8lqih9ze.execute-api.us-east-1.amazonaws.com/stage/api/company';
+  private baseUrl = 'https://44n9fvhnl0.execute-api.us-east-1.amazonaws.com/api/company';
 
   constructor(private http: HttpClient) {
   }

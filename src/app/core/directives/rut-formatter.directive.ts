1876d35927e089bd750958fea3b6d8e1a3b228b3
Héctor Mora <hmora1997gmail.com>
2024-07-12 00:15:07 -0400

    ajustar rut formato y solo letra k

--- src/app/core/directives/rut-formatter.directive.ts
+++ src/app/core/directives/rut-formatter.directive.ts
@@ -1,26 +1,29 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { RutPipe } from '../pipes/rut.pipe';
 
 @Directive({
   selector: '[appRutFormatter]',
   standalone: true
 })
 export class RutFormatterDirective {
-  constructor(private el: ElementRef) {
-  }
+  private rutPipe = new RutPipe();
 
-  @HostListener('blur', [ '$event' ])
+  constructor(private el: ElementRef) {}
+
+  @HostListener('blur', ['$event'])
   onBlur(event: KeyboardEvent) {
     let value = this.el.nativeElement.value;
-    value = this.formatRut(value);
+    value = this.rutPipe.transform(value);
     this.el.nativeElement.value = value;
   }
 
-  private formatRut(value: string): string {
-    let rut = value.replace(/\./g, '').replace('-', '');
-    if ( rut.length > 1 ) {
-      rut = rut.substring(0, rut.length - 1).replace(/\B(?=(\d{3})+(?!\d))/g, '.') + '-' + rut.substring(rut.length - 1);
+  @HostListener('input', ['$event'])
+  onInput(event: KeyboardEvent) {
+    let value = this.el.nativeElement.value;
+    value = value.replace(/[^0-9kK]/g, '');
+    if (value.length > 9) { // 8 digits + 1 verifier character
+      value = value.substring(0, 9);
     }
-    return rut;
+    this.el.nativeElement.value = value;
   }
-
 }
--- src/app/core/pipes/rut.pipe.ts
+++ src/app/core/pipes/rut.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'rutPipe',
@@ -7,9 +7,13 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class RutPipe implements PipeTransform {
   transform(value: string): string {
-    if ( !value ) return value;
-    let rut = value.replace(/./g, '').replace('-', '');
-    if ( rut.length > 1 ) {
+    if (!value) return value;
+    let rut = value.replace(/[^0-9kK]/g, '').toLowerCase();
+    if (rut.length > 9) {
+      rut = rut.substring(0, 9);
+    }
+    rut = rut.replace(/\./g, '').replace('-', '');
+    if (rut.length > 1) {
       rut = rut.substring(0, rut.length - 1).replace(/\B(?=(\d{3})+(?!\d))/g, '.') + '-' + rut.substring(rut.length - 1);
     }
     return rut;
